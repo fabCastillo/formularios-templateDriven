f50fbe99a25a28de45e0d384f8d77898c26a007c
Fabian Castillo <fabianboc960@hotmail.com>
2022-08-31 23:29:53 -0500

    Custom Directive: custom-min

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { TemplateRoutingModule } from './template-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
+import { CustomMinDirective } from './directives/custom-min.directive';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
+import { TemplateRoutingModule } from './template-routing.module';
 import { SwitchesComponent } from './switches/switches.component';
 
 
 @NgModule({
   declarations: [
     BasicosComponent,
+    CustomMinDirective,
     DinamicosComponent,
     SwitchesComponent
   ],
