f9332b16d58af315d7a98cb8e6de6c82792b3aad
Fabian Castillo <fabianboc960@hotmail.com>
2022-09-20 21:40:41 -0500

    Formularios Reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent  {
 
-  constructor() { }
+  // myForm: FormGroup = new FormGroup({
+  //   nombre     : new FormControl('Mouse inalambrico'),
+  //   precio     : new FormControl(1500),
+  //   existencias: new FormControl(5)
+  // })
 
-  ngOnInit(): void {
+  myForm: FormGroup = this.formBuilder.group({
+    nombre     : [ null, [ Validators.required, Validators.minLength(3) ] ],
+    precio     : [ null, [ Validators.required, Validators.min(0) ] ],
+    existencias: [ null, [ Validators.required, Validators.min(0) ] ]
+  });
+  
+  constructor( private formBuilder: FormBuilder ) { }
+
+  public fieldIsNotValid( field: string ) {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
   }
 
 }
